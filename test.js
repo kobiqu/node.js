deb15ab7afa48661331ef592e30ca9aacb0d4838
kobiqu <139848127+kobiqu@users.noreply.github.com>
2023-08-17 12:28:42 +0300

    Update test.js
    build server 1

--- test.js
+++ test.js
@@ -2,17 +2,25 @@ const express = require("express");
 const app = express();
 const uuid = require("uuid");
 const bcrypt = require("bcrypt");
+
 const myPlaintextPassword = "s0//P4$$w0rD";
 const someOtherPlaintextPassword = "not_bacon";
+const saltRounds = 10;
+
+// items db
 const person = [
-  { id: "1", email: "david@gmail.com", password: "6746637" },
-  { id: "2", email: "shmuel@gmail.com", password: "0957464" },
-  { id: "3", email: "kobi@gmail.com", password: "876353" },
+  { id: uuid.v4(), email: "david@gmail.com", password: "6746637" },
+  { id: uuid.v4(), email: "shmuel@gmail.com", password: "0957464" },
+  { id: uuid.v4(), email: "kobi@gmail.com", password: "876353" },
 ];
+
 app.use(express.json());
+
+// get the db by req
 app.get("/kobi", (req, res) => {
   res.send(person);
 });
+
 // search user by is id
 app.get("/kobi/:id", (req, res) => {
   person.forEach((item) => {
@@ -21,18 +29,21 @@ app.get("/kobi/:id", (req, res) => {
     }
   });
 });
+
 // create new user + unique id and password
 app.post("/kobi/newUser", (req, res) => {
   let new_id = (req.body.id = uuid.v4());
-  let cryptedPassword = bcrypt.hash(req.body.password, 10);
-  const p = req.body;
+  let cryptedPassword = bcrypt.hashSync(req.body.password, 10);
   person.push({
     id: new_id,
+    email: req.body.email,
     password: cryptedPassword,
   });
 
   res.send(person);
+  setTimeout(() => console.log("user have been created successfully"), 2000);
 });
+
 // update user details
 app.put("/updateUser/:id", (req, res) => {
   const place = req.params.id;
@@ -41,42 +52,30 @@ app.put("/updateUser/:id", (req, res) => {
 
   res.send(person);
 });
+
 // delete user
 app.get("/:id", (req, res) => {
   delete person[req.params.id];
   res.send(person);
 });
+
 // check email and password
 app.post("/login", (req, res) => {
-  if (
-    req.body.email == person[1].email &&
-    req.body.password == person[1].password
-  ) {
-    res.send("you are connected");
-  } else {
-    res.send("error you are not sign");
+  const users = req.body;
+  try {
+    let a = person.find((user) => users.email === user.email);
+    let b = person.filter((user) => users.email === user.email);
+    if (a) {
+      res.send(person);
+      console.log(`success!! the user just enter to the system`);
+    } else {
+      throw new Error("you are not sign");
+    }
+  } catch (error) {
+    res.send(error.message);
   }
 });
-bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
-  let hashPassword = hash;
-});
-bcrypt.compare(myPlaintextPassword, saltRounds, function (err, result) {
-  result = true;
-});
-bcrypt.compare(someOtherPlaintextPassword, saltRounds, function (err, result) {
-  result = false;
-});
-async function checkUser(username, password) {
-  //... fetch user from a db etc.
 
-  const match = await bcrypt.compare(password, user.passwordHash);
-
-  if (match) {
-    //login
-  }
-
-  //...
-}
 app.listen(3000, () => {
   console.log(`server is up and running on port:${3000}`);
 });
